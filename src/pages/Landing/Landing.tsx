67befac2f340d562292f9a8861f367d053c2cc1e
SebastianPrario <121460546+SebastianPrario@users.noreply.github.com>
2025-08-20 20:33:24 -0300

    modificacion en resultado de consulta cuando no hay info

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -66,7 +66,7 @@ export default function Landing() {
               loading = {loading}
               />
             </div>
-              {data ? (
+              {data && (
                 <ResultModal 
                   show={show}
                   handleClose={handleClose}>
@@ -77,7 +77,7 @@ export default function Landing() {
                   />
                   )}
                 </ResultModal>
-              ): <>no hay datos para esta consulta</>}
+              )}
               
            </div>
           <Footer/>
